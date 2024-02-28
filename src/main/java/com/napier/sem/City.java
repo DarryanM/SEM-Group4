a119dae4cc0b247520597a1dca97328ba96b1883
Sheneef <40671084@live.napier.ac.uk>
2024-02-27 21:13:40 -0400

    City Class

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,16 @@
+package com.napier.sem;
+
+public class City {
+    /**
+     * City Population
+     */
+    public int population;
+    /**
+     * City Information
+     */
+    public String name;
+
+    public String countrycode ;
+
+    public String district ;
+}
